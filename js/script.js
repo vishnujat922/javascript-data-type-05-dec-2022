b5a4a84b667e510c61e8ee776aca64000baa5b65
vishnujat922 <vishnujat922@gmail.com>
2022-12-06 00:13:16 +0530

    js data type

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// single line comments
+/*
+this is Multiline comment
+lets take a js object for storing student information
+*/
+var student1 = {
+    //1 property:value
+    name:"sampat",
+    surname:"prajapati"
+    //2
+
+}
+
+// how many way I can display the output of a js code
+//1 console.log
+// object.propertyname
+console.log("hello "+ student1.name)//this + is the concatination operator
